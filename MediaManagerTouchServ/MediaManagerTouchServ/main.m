317881eb06b51cbf46acdcb39fc42fcd112a0217
Marek Ryniejski <marekjulian@gmail.com>
2012-12-05 17:01:46 -0800

    Initial checkin of MediaManagerTouchServ

---
+++ MediaManagerTouchServ/MediaManagerTouchServ/main.m
@@ -0,0 +1,191 @@
+//
+//  main.m
+//  MediaManagerTouchServ
+//
+//  Created by Marek Ryniejski on 12/5/12.
+//  Copyright (c) 2012 Marek Ryniejski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <TouchDB/TD_DatabaseManager.h>
+#import <TouchDB/TDRouter.h>
+#import <TouchDBListener/TDListener.h>
+#import <CouchCocoa/CouchCocoa.h>
+
+#define kPortNumber 59840
+
+static NSString* GetServerPath() {
+    NSString* bundleID = [[NSBundle mainBundle] bundleIdentifier];
+    if (!bundleID)
+        bundleID = @"com.jetsonsystems.MediaManagerTouchServ";
+    
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(
+        NSApplicationSupportDirectory,
+        NSUserDomainMask, YES);
+    NSString* path = paths[0];
+    path = [path stringByAppendingPathComponent: bundleID];
+    path = [path stringByAppendingPathComponent: @"MediaManagerTouchDB"];
+    NSError* error = nil;
+    if (![[NSFileManager defaultManager] createDirectoryAtPath: path
+        withIntermediateDirectories: YES
+        attributes: nil error: &error]) {
+        NSLog(@"FATAL: Couldn't create TouchDB server dir at %@", path);
+        exit(1);
+    }
+    return path;
+}
+
+//
+// main: Main program.
+//
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+
+        TD_DatabaseManagerOptions options = kTD_DatabaseManagerDefaultOptions;
+        options.readOnly = NO;
+        const char* replArg = NULL, *user = NULL, *password = NULL;
+        BOOL auth = NO, pull = NO, createTarget = NO, continuous = NO;
+                
+        for (int i = 1; i < argc; ++i) {
+            if (strcmp(argv[i], "--readonly") == 0) {
+                options.readOnly = YES;
+            } else if (strcmp(argv[i], "--auth") == 0) {
+                auth = YES;
+            } else if (strcmp(argv[i], "--pull") == 0) {
+                replArg = argv[++i];
+                pull = YES;
+            } else if (strcmp(argv[i], "--push") == 0) {
+                replArg = argv[++i];
+            } else if (strcmp(argv[i], "--create-target") == 0) {
+                createTarget = YES;
+            } else if (strcmp(argv[i], "--continuous") == 0) {
+                continuous = YES;
+            } else if (strcmp(argv[i], "--user") == 0) {
+                user = argv[++i];
+            } else if (strcmp(argv[i], "--password") == 0) {
+                password = argv[++i];
+            }
+        }
+                
+        CouchTouchDBServer* server = [[CouchTouchDBServer alloc]
+                                      initWithServerPath: GetServerPath()];
+                
+        NSString* dbName = @"plm-media-manager-test0";
+        CouchDatabase* database = [server databaseNamed: dbName];
+                
+        RESTOperation* op = [database create];
+        if (![op wait]) {
+            NSCAssert(op.error.code == 412, @"Error creating db!");
+        }
+        
+        NSLog(@"MediaManagerTouchServ created database: %@!",
+              dbName);
+                
+        // Setup up our design document.
+        CouchDesignDocument* design =
+            [database designDocumentWithName: @"_design/plm-image"];
+        [design defineViewNamed: @"by_oid_with_variant" mapBlock: MAPBLOCK({
+            NSString* oid;
+            NSNumber* isDerived;
+            NSNumber* docWidth = [[doc objectForKey: @"size"] objectForKey: @"width"];
+            
+            if ([[doc objectForKey: @"orig_id"] isEqualToString:@""]) {
+                oid = doc[@"oid"];
+                isDerived = [NSNumber numberWithInteger: 0];
+            }
+            else {
+                oid = doc[@"orig_id"];
+                isDerived = [NSNumber numberWithInteger: 1];
+            }
+            emit([NSArray arrayWithObjects:oid, isDerived, docWidth, nil],
+                 doc[@"path"]);
+        }) version: @"0.0.1"];
+        [design defineViewNamed: @"by_creation_time" mapBlock: MAPBLOCK({
+            if ([[doc objectForKey: @"class_name"]
+                 isEqualToString:@"plm.Image"]) {
+                id docCreatedAt = doc[@"created_at"];
+                //
+                // build the key:
+                //
+                //   note created_at is in this format: 2012-12-05T01:37:55.087Z
+                //
+                //   key format: <key> ::= [<date part>,<image part>]
+                //
+                //     <date part> ::= <year>,<month>,<day>,<hours>,<minutes>,<seconds>,<milliseconds>
+                //
+                //     <image part> ::= <original image part> | <derived image part>
+                //     <original image part> ::= doc.oid,0,doc.size.width
+                //     <derived image part>  ::= doc.orig_id,1,doc.size.width
+                //
+                NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+                [dateFormat setDateFormat:@"YYYY'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"];
+                NSDate *date = [dateFormat dateFromString:docCreatedAt];
+                NSCalendar* calendar = [NSCalendar currentCalendar];
+                NSDateComponents* components = [calendar
+                                                components:
+                                                (NSYearCalendarUnit|
+                                                 NSMonthCalendarUnit|
+                                                 NSDayCalendarUnit|
+                                                 NSHourCalendarUnit|
+                                                 NSMinuteCalendarUnit|
+                                                 NSSecondCalendarUnit) fromDate:date];
+                NSNumber* year = [NSNumber numberWithInteger:[components year]];
+                NSNumber* month = [NSNumber numberWithInteger:[components month]];
+                NSNumber* day = [NSNumber numberWithInteger: [components day]];
+                NSNumber* hour = [NSNumber numberWithInteger: [components hour]];
+                NSNumber* minute = [NSNumber numberWithInteger: [components minute]];
+                NSNumber* second = [NSNumber numberWithInteger: [components second]];
+                NSNumber* milliseconds = [NSNumber numberWithInteger: 0];
+                        
+                NSString* oid;
+                NSNumber* isDerived;
+                NSNumber* docWidth = [[doc objectForKey: @"size"] objectForKey: @"width"];
+                        
+                if ([[doc objectForKey: @"orig_id"] isEqualToString:@""]) {
+                    oid = doc[@"oid"];
+                    isDerived = [NSNumber numberWithInteger: 0];
+                }
+                else {
+                    oid = doc[@"orig_id"];
+                    isDerived = [NSNumber numberWithInteger: 1];
+                }
+                        
+                id docPath = doc[@"path"];
+                emit([NSArray arrayWithObjects:
+                      year,
+                      month,
+                      day,
+                      hour,
+                      minute,
+                      second,
+                      milliseconds,
+                      oid,
+                      isDerived,
+                      docWidth,
+                      nil],
+                     docPath);
+            }
+        }) version: @"0.0.5"];
+        [design saveChanges];
+                
+        // Start a listener socket:
+        [server tellTDServer: ^(TD_Server* tdServer) {
+            TDListener* listener = [[TDListener alloc] initWithTDServer: tdServer port: kPortNumber];
+            listener.readOnly = options.readOnly;
+            [listener start];
+        }];
+        NSLog(@"MediaManagerTouchServ %@ is listening%@ on port %d ... relax!",
+              [TDRouter versionString],
+              (options.readOnly ? @" in read-only mode" : @""),
+              kPortNumber);
+                
+        [[NSRunLoop currentRunLoop] run];
+                
+        NSLog(@"MediaManagerTouchServ quitting");
+        
+    }
+    return 0;
+}
+
---
+++ MediaManagerTouchServ/MediaManagerTouchServerTests/MediaManagerTouchServerTests.m
@@ -0,0 +1,32 @@
+//
+//  MediaManagerTouchServerTests.m
+//  MediaManagerTouchServerTests
+//
+//  Created by Marek Ryniejski on 12/5/12.
+//  Copyright (c) 2012 Marek Ryniejski. All rights reserved.
+//
+
+#import "MediaManagerTouchServerTests.h"
+
+@implementation MediaManagerTouchServerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MediaManagerTouchServerTests");
+}
+
+@end
