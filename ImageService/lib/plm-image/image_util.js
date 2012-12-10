812a7b6c9d75df485f5a6a8c41048777456ec773
Philippe Paravicini <philippe@paravicini.com>
2012-12-10 10:53:51 -0800

    fix bug in image_util.js that came to fore in unit test

--- ImageService/lib/plm-image/image_util.js
+++ ImageService/lib/plm-image/image_util.js
@@ -5,7 +5,7 @@ var _ = require('underscore');
 // if only the width or height is provided, the new size will fit to that width or height and
 // preserve the aspect ratio; if both width and height are provided, the new size will be
 // exactly as requested, which may distort the image
-exports.fitToSize = function (size, newSize) {
+function fitToSize(size, newSize) {
   if (newSize.width > 0) {
     if (newSize.height > 0) {
       return newSize;
@@ -18,50 +18,41 @@ exports.fitToSize = function (size, newSize) {
     throw "fitToSize new size is improperly defined";
   }
 }
+exports.fitToSize = fitToSize;
+
 
 // utility class that computes the new size for an image so that its longest size will be numPix
 // long; so for example, if the image is 300x150 and numPix is 100, the computed image size will be
 // 100x50, and if the image is 150x300 it will be 50x100
-exports.fitToSquare = function (size, numPix) 
+function fitToSquare(size, numPix) 
 {
-  /*
-  if (_.isObject(size)) {
-    if (_.isNumber(size.width) && _.isNumber(size.height) && size.width > 0 && size.height > 0) {
-      var aspect_ratio = size.width / size.height;
-      var out = {};
-      if (aspect_ratio >= 1) {
-        out.width  = numPix;
-        out.height = Math.round(numPix/aspect_ratio);
-      } else {
-        out.width  = Math.round(numPix*aspect_ratio);
-        out.height = numPix;
-      }
-      return out;
-    }
-  } 
-  throw "size is not properly defined";
-  */
   var aspect_ratio = exports.aspectRatio(size);
   return (aspect_ratio >= 1) ? 
     exports.fitToWidth(size, numPix) : exports.fitToHeight(size, numPix);
 }
+exports.fitToSquare = fitToSquare;
+
 
 // returns a new size that preserves the aspect ratio based on the pixel height
-exports.fitToHeight = function (size, pixHeight) {
+function fitToHeight(size, pixHeight) {
   var out = {};
   out.width  = Math.round( pixHeight*exports.aspectRatio(size) );
   out.height = pixHeight;
   return out;
 }
+exports.fitToHeight = fitToHeight;
 
-exports.fitToWidth = function (size, pixWidth) {
+
+function fitToWidth(size, pixWidth) {
   var out = {};
   out.width  = pixWidth;
-  out.height = Math.round(numPix/aspect_ratio);
+  out.height = Math.round(pixWidth/aspectRatio(size));
   return out;
 }
+exports.fitToWidth = fitToWidth;
 
-exports.aspectRatio = function (size) {
+
+function aspectRatio(size) {
   if (_.isObject(size)) {
     if (_.isNumber(size.width) && _.isNumber(size.height) && size.width > 0 && size.height > 0) {
       return (size.width / size.height);
@@ -69,4 +60,5 @@ exports.aspectRatio = function (size) {
   }
   throw "size is not properly defined";
 }
+exports.aspectRatio = aspectRatio;
 
