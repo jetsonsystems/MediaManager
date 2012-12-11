cef68cd9a355e171cb27a8b1a416f848700ba3ea
Marek Ryniejski <marekjulian@gmail.com>
2012-12-10 20:42:38 -0800

    Initial app config / init.

---
+++ MediaManagerAppConfig/lib/config.js
@@ -0,0 +1,15 @@
+var config = {
+    db: {
+	database: "plm-media-manager",
+	local: {
+	    port: "59840"
+	},
+	remote: {
+	    host: "localhost",
+	    port: "5984"
+	}
+    }
+};
+
+module.exports = config;
+
