3f1c4d38bfd544cea8c41b072718a228b8aece9a
Daniel Mora <moradaniel@gmail.com>
2012-12-17 10:03:18 -0300

    Merging

--- MediaManagerTouchServ/MediaManagerTouchServ/main.m
+++ MediaManagerTouchServ/MediaManagerTouchServ/main.m
@@ -91,6 +91,7 @@ int main(int argc, const char * argv[])
         // Setup up our design document.
         CouchDesignDocument* design =
             [database designDocumentWithName: @"_design/plm-image"];
+        
         [design defineViewNamed: @"by_oid_with_variant" mapBlock: MAPBLOCK({
             NSString* oid;
             NSNumber* isDerived;
@@ -107,6 +108,7 @@ int main(int argc, const char * argv[])
             emit([NSArray arrayWithObjects:oid, isDerived, docWidth, nil],
                  doc[@"path"]);
         }) version: @"0.0.1"];
+        
         [design defineViewNamed: @"by_creation_time" mapBlock: MAPBLOCK({
             if ([[doc objectForKey: @"class_name"]
                  isEqualToString:@"plm.Image"]) {
@@ -173,6 +175,104 @@ int main(int argc, const char * argv[])
                      docPath);
             }
         }) version: @"0.0.5"];
+        
+        [design defineViewNamed: @"batch_by_ctime" mapBlock: MAPBLOCK({
+            if ([[doc objectForKey: @"class_name"]
+                 isEqualToString:@"plm.ImportBatch"]) {
+                id docCreatedAt = doc[@"created_at"];
+                //
+                // build the key:
+                //
+                //   note created_at is in this format: 2012-12-05T01:37:55.087Z
+                //
+                //   key format: <key> ::= [<date part>]
+                //
+                //     <date part> ::=
+                //       <year>,<month>,<day>,<hours>,<minutes>,<seconds>,<milliseconds>
+                //
+                NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+                [dateFormat setDateFormat:@"YYYY'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"];
+                NSDate *date = [dateFormat dateFromString:docCreatedAt];
+                NSCalendar* calendar = [NSCalendar currentCalendar];
+                NSDateComponents* components = [calendar
+                                                components:
+                                                (NSYearCalendarUnit|
+                                                 NSMonthCalendarUnit|
+                                                 NSDayCalendarUnit|
+                                                 NSHourCalendarUnit|
+                                                 NSMinuteCalendarUnit|
+                                                 NSSecondCalendarUnit) fromDate:date];
+                NSNumber* year = [NSNumber numberWithInteger:[components year]];
+                NSNumber* month = [NSNumber numberWithInteger:[components month]];
+                NSNumber* day = [NSNumber numberWithInteger: [components day]];
+                NSNumber* hour = [NSNumber numberWithInteger: [components hour]];
+                NSNumber* minute = [NSNumber numberWithInteger: [components minute]];
+                NSNumber* second = [NSNumber numberWithInteger: [components second]];
+                NSNumber* milliseconds = [NSNumber numberWithInteger: 0];
+
+                id docPath = doc[@"path"];
+                emit([NSArray arrayWithObjects:
+                      year,
+                      month,
+                      day,
+                      hour,
+                      minute,
+                      second,
+                      milliseconds,
+                      nil],
+                     docPath);
+            }
+        }) version: @"0.0.0"];
+        
+        [design defineViewNamed: @"batch_by_oid_w_image" mapBlock: MAPBLOCK({
+            if ([[doc objectForKey: @"class_name"]
+                 isEqualToString:@"plm.ImportBatch"]) {
+                //
+                //  emit([doc.oid, '0', 0, 0], doc.path)
+                //
+                NSString* oid = doc[@"oid"];
+                id docPath = doc[@"path"];
+                emit([NSArray arrayWithObjects:
+                      oid,
+                      @"0",
+                      [NSNumber numberWithInteger: 0],
+                      [NSNumber numberWithInteger: 0],
+                      nil],
+                     docPath);
+            }
+            else if ([[doc objectForKey: @"class_name"]
+                     isEqualToString:@"plm.Import"]) {
+                //
+                //  original doc:
+                //    emit([doc.batch_id, doc.oid, 1, doc.size.width], doc.path)
+                //
+                //  variant:
+                //    emit([doc.batch_id, doc.orig_id, 2, doc.size.width], doc.path)
+                //
+                NSString* batchId = doc[@"batch_id"];
+                NSString* oid;
+                NSNumber* originalFlag;
+                NSNumber* docWidth = [[doc objectForKey: @"size"] objectForKey: @"width"];
+                
+                if ([[doc objectForKey: @"orig_id"] isEqualToString:@""]) {
+                    oid = doc[@"oid"];
+                    originalFlag = [NSNumber numberWithInteger: 1];
+                }
+                else {
+                    oid = doc[@"orig_id"];
+                    originalFlag = [NSNumber numberWithInteger: 2];
+                }
+                id docPath = doc[@"path"];
+                emit([NSArray arrayWithObjects:
+                      batchId,
+                      oid,
+                      originalFlag,
+                      docWidth,
+                      nil],
+                     docPath);
+            }
+        }) version: @"0.0.0"];
+        
         [design saveChanges];
                 
         // Start a listener socket:
