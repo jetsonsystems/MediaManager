7d154116309ec6a5db65b7469d9e12be9b8ea75b
Daniel Mora <moradaniel@gmail.com>
2012-12-20 17:02:07 -0300

    Initial test for the MediaManagerApi

--- ImageService/test/image_util_test.js
+++ ImageService/test/image_util_test.js
@@ -1,40 +1,40 @@
 var
-    should = require("should")
-    , image_util = require("../lib/plm-image/image_util");
+  should = require("should")
+  , image_util = require("../lib/plm-image/image_util");
 
 describe('fitToSize tests:', function () {
 
-    var currentSize = {width:20, height:40};
+  var currentSize = {width:20, height:40};
 
-    it("given height of 100 fitToSize should give width of 50", function () {
-        var proposedNewSize = {height:100};
-        var newSize = image_util.fitToSize(currentSize, proposedNewSize);
-        newSize.width.should.equal(50);
-    });
-
-
-     it("given width of 50 fitToSize should give height of 100", function () {
-     var proposedNewSize = {width:50};
-     var newSize = image_util.fitToSize(currentSize, proposedNewSize);
-     newSize.height.should.equal(100);
-     });
+  it("given height of 100 fitToSize should give width of 50", function () {
+    var proposedNewSize = {height:100};
+    var newSize = image_util.fitToSize(currentSize, proposedNewSize);
+    newSize.width.should.equal(50);
+  });
 
 
-    it("given height and width the size returned by fitToSize should be equal to the given size", function () {
-        var proposedNewSize = {width:50, height:100};
-        var newSize = image_util.fitToSize(currentSize, proposedNewSize);
-        newSize.width.should.equal(proposedNewSize.width);
-        newSize.height.should.equal(proposedNewSize.height);
-    });
+  it("given width of 50 fitToSize should give height of 100", function () {
+    var proposedNewSize = {width:50};
+    var newSize = image_util.fitToSize(currentSize, proposedNewSize);
+    newSize.height.should.equal(100);
+  });
 
-    it("Should throw exception due to malformed size", function () {
-            (function () {
-                var malformedSize = {width:-20, height:-40};
-                var newSize = image_util.fitToSize(currentSize, malformedSize);
-            }).should.throw();
-        }
 
-    );
+  it("given height and width the size returned by fitToSize should be equal to the given size", function () {
+    var proposedNewSize = {width:50, height:100};
+    var newSize = image_util.fitToSize(currentSize, proposedNewSize);
+    newSize.width.should.equal(proposedNewSize.width);
+    newSize.height.should.equal(proposedNewSize.height);
+  });
+
+  it("Should throw exception due to malformed size", function () {
+      (function () {
+        var malformedSize = {width:-20, height:-40};
+        var newSize = image_util.fitToSize(currentSize, malformedSize);
+      }).should.throw();
+    }
+
+  );
 
 });
 
@@ -42,79 +42,79 @@ describe('fitToSize tests:', function () {
 describe('fitToSquare tests:', function () {
 
 
-    it("given size 300x150 and numPix of 100 fitToSquare should give size of 100x50", function () {
-        var currentSize = {width:300, height:150};
-        var numPix = 100;
-        var newSize = image_util.fitToSquare(currentSize, numPix);
-        newSize.width.should.equal(100);
-        newSize.height.should.equal(50);
-    });
+  it("given size 300x150 and numPix of 100 fitToSquare should give size of 100x50", function () {
+    var currentSize = {width:300, height:150};
+    var numPix = 100;
+    var newSize = image_util.fitToSquare(currentSize, numPix);
+    newSize.width.should.equal(100);
+    newSize.height.should.equal(50);
+  });
 
-    it("given size 150x300 and numPix of 100 fitToSquare should give size of 50x100", function () {
-        var currentSize = {width:150, height:300};
-        var numPix = 100;
-        var newSize = image_util.fitToSquare(currentSize, numPix);
-        newSize.width.should.equal(50);
-        newSize.height.should.equal(100);
-    });
+  it("given size 150x300 and numPix of 100 fitToSquare should give size of 50x100", function () {
+    var currentSize = {width:150, height:300};
+    var numPix = 100;
+    var newSize = image_util.fitToSquare(currentSize, numPix);
+    newSize.width.should.equal(50);
+    newSize.height.should.equal(100);
+  });
 
 });
 
 describe('fitToHeight tests:', function () {
 
 
-    it("given size 300x150 and numPix of 100 fitToHeight should give size of 200x100", function () {
-        var currentSize = {width:300, height:150};
-        var numPix = 100;
-        var newSize = image_util.fitToHeight(currentSize, numPix);
-        newSize.width.should.equal(200);
-        newSize.height.should.equal(100);
-    });
+  it("given size 300x150 and numPix of 100 fitToHeight should give size of 200x100", function () {
+    var currentSize = {width:300, height:150};
+    var numPix = 100;
+    var newSize = image_util.fitToHeight(currentSize, numPix);
+    newSize.width.should.equal(200);
+    newSize.height.should.equal(100);
+  });
 
-    it("given size 150x300 and numPix of 100 fitToHeight should give size of 50x100", function () {
-        var currentSize = {width:150, height:300};
-        var numPix = 100;
-        var newSize = image_util.fitToHeight(currentSize, numPix);
-        newSize.width.should.equal(50);
-        newSize.height.should.equal(100);
-    });
+  it("given size 150x300 and numPix of 100 fitToHeight should give size of 50x100", function () {
+    var currentSize = {width:150, height:300};
+    var numPix = 100;
+    var newSize = image_util.fitToHeight(currentSize, numPix);
+    newSize.width.should.equal(50);
+    newSize.height.should.equal(100);
+  });
 
 });
 
 describe('fitToWidth tests:', function () {
 
 
-    it("given size 300x150 and numPix of 100 fitToWidth should give size of 100x50", function () {
-        var currentSize = {width:300, height:150};
-        var numPix = 100;
-        var newSize = image_util.fitToWidth(currentSize, numPix);
-        newSize.width.should.equal(100);
-        newSize.height.should.equal(50);
-    });
+  it("given size 300x150 and numPix of 100 fitToWidth should give size of 100x50", function () {
+    var currentSize = {width:300, height:150};
+    var numPix = 100;
+    var newSize = image_util.fitToWidth(currentSize, numPix);
+    newSize.width.should.equal(100);
+    newSize.height.should.equal(50);
+  });
 
-    it("given size 150x300 and numPix of 100 fitToWidth should give size of 100x200", function () {
-        var currentSize = {width:150, height:300};
-        var numPix = 100;
-        var newSize = image_util.fitToWidth(currentSize, numPix);
-        newSize.width.should.equal(100);
-        newSize.height.should.equal(200);
-    });
+  it("given size 150x300 and numPix of 100 fitToWidth should give size of 100x200", function () {
+    var currentSize = {width:150, height:300};
+    var numPix = 100;
+    var newSize = image_util.fitToWidth(currentSize, numPix);
+    newSize.width.should.equal(100);
+    newSize.height.should.equal(200);
+  });
 
 });
 
 describe('aspectRatio tests:', function () {
 
 
-    it("given size 300x150 aspectRatio should be equal to 2", function () {
-        var currentSize = {width:300, height:150};
-        var aspectRatio = image_util.aspectRatio(currentSize);
-        aspectRatio.should.equal(2);
-    });
+  it("given size 300x150 aspectRatio should be equal to 2", function () {
+    var currentSize = {width:300, height:150};
+    var aspectRatio = image_util.aspectRatio(currentSize);
+    aspectRatio.should.equal(2);
+  });
 
-    it("given size 150x300  aspectRatio should be equal to 0.5", function () {
-        var currentSize = {width:150, height:300};
-        var aspectRatio = image_util.aspectRatio(currentSize);
-        aspectRatio.should.equal(0.5);
-    });
+  it("given size 150x300  aspectRatio should be equal to 0.5", function () {
+    var currentSize = {width:150, height:300};
+    var aspectRatio = image_util.aspectRatio(currentSize);
+    aspectRatio.should.equal(0.5);
+  });
 
 });
--- ImageService/test/update_design_doc.js
+++ ImageService/test/update_design_doc.js
@@ -1,103 +1,102 @@
 'use strict';
 
 var
-    nano = require('nano')
-   ,async = require('async');
+  nano = require('nano')
+  , async = require('async');
 
 
-exports.updateDesignDoc=updateDesignDoc;
+exports.updateDesignDoc = updateDesignDoc;
 
 function updateDesignDoc(options, callback) {
 
-    var db = nano('http://' + options.host + ':' + options.port + '/' + options.dbName);
+  var db = nano('http://' + options.host + ':' + options.port + '/' + options.dbName);
 
-    console.log('Updating db: host - ' + options.host+ ', port - ' + options.port + ', db name - ' + options.dbName);
+  console.log('Updating db: host - ' + options.host + ', port - ' + options.port + ', db name - ' + options.dbName);
 
 
-    var design_doc_id = '_design/plm-image';
+  var design_doc_id = '_design/plm-image';
 
-    var design_doc_couchdb = {
-        "views":{
-            "by_oid_with_variant":{
-                "map":"function(doc) { if (doc.class_name === 'plm.Image') { var key; if (doc.orig_id === ''){ key = [doc.oid,0,doc.size.width]; emit(key, doc.path) } else { key = [doc.orig_id,1,doc.size.width]; emit(key, doc.name)}} }"
-            }, "by_creation_time":{
-                "map":"function(doc) { if (doc.class_name === 'plm.Image') { var key = date_to_array(doc.created_at); if (doc.orig_id === '') { key.push(doc.oid,0,doc.size.width); emit(key,doc.path)} else { key.push(doc.orig_id,1,doc.size.width); emit(key,doc.name) }} function date_to_array(aDate) { var out = [], d = new Date(aDate); out.push(d.getFullYear()); out.push(d.getMonth()+1); out.push(d.getDate()); out.push(d.getHours()); out.push(d.getMinutes()); out.push(d.getSeconds()); out.push(d.getMilliseconds()); return out;} }"
-            }, "batch_by_ctime":{
-                "map":"function(doc) { if (doc.class_name === 'plm.ImportBatch') { var key = date_to_array(doc.created_at); key.push(doc.oid,0); emit(key,doc.path); } function date_to_array(aDate) { var out = [], d = new Date(aDate); out.push(d.getFullYear()); out.push(d.getMonth()+1); out.push(d.getDate()); out.push(d.getHours()); out.push(d.getMinutes()); out.push(d.getSeconds()); out.push(d.getMilliseconds()); return out; }}"
-            }, "batch_by_oid_w_image":{
-                "map":"function(doc) { var key = []; if (doc.class_name === 'plm.ImportBatch') { key.push(doc.oid,'0',0,0); emit(key,doc.path); } if (doc.class_name === 'plm.Image') { if (doc.orig_id === '') { key.push(doc.batch_id, doc.oid, 1, doc.size.width);} else { key.push(doc.batch_id, doc.orig_id, 2, doc.size.width); } emit(key,doc.name) } function date_to_array(aDate) { var out = [], d = new Date(aDate); out.push(d.getFullYear()); out.push(d.getMonth()+1); out.push(d.getDate()); out.push(d.getHours()); out.push(d.getMinutes()); out.push(d.getSeconds()); out.push(d.getMilliseconds()); return out; }}"
-            }
-        }
-    };
-
-    var design_doc_touchdb = {
-        "views":{
-            "image_by_oid_w_variant":{
-            }, "image_by_ctime":{
-            }, "batch_by_ctime":{
-            }, "batch_by_oid_w_image":{
-            }
-        }
-    };
-
-    var design_doc;
-
-    if (options.design_doc === 'couchdb') {
-        console.log('DB type is couchdb');
-        design_doc = design_doc_couchdb;
+  var design_doc_couchdb = {
+    "views":{
+      "by_oid_with_variant":{
+        "map":"function(doc) { if (doc.class_name === 'plm.Image') { var key; if (doc.orig_id === ''){ key = [doc.oid,0,doc.size.width]; emit(key, doc.path) } else { key = [doc.orig_id,1,doc.size.width]; emit(key, doc.name)}} }"
+      }, "by_creation_time":{
+        "map":"function(doc) { if (doc.class_name === 'plm.Image') { var key = date_to_array(doc.created_at); if (doc.orig_id === '') { key.push(doc.oid,0,doc.size.width); emit(key,doc.path)} else { key.push(doc.orig_id,1,doc.size.width); emit(key,doc.name) }} function date_to_array(aDate) { var out = [], d = new Date(aDate); out.push(d.getFullYear()); out.push(d.getMonth()+1); out.push(d.getDate()); out.push(d.getHours()); out.push(d.getMinutes()); out.push(d.getSeconds()); out.push(d.getMilliseconds()); return out;} }"
+      }, "batch_by_ctime":{
+        "map":"function(doc) { if (doc.class_name === 'plm.ImportBatch') { var key = date_to_array(doc.created_at); key.push(doc.oid,0); emit(key,doc.path); } function date_to_array(aDate) { var out = [], d = new Date(aDate); out.push(d.getFullYear()); out.push(d.getMonth()+1); out.push(d.getDate()); out.push(d.getHours()); out.push(d.getMinutes()); out.push(d.getSeconds()); out.push(d.getMilliseconds()); return out; }}"
+      }, "batch_by_oid_w_image":{
+        "map":"function(doc) { var key = []; if (doc.class_name === 'plm.ImportBatch') { key.push(doc.oid,'0',0,0); emit(key,doc.path); } if (doc.class_name === 'plm.Image') { if (doc.orig_id === '') { key.push(doc.batch_id, doc.oid, 1, doc.size.width);} else { key.push(doc.batch_id, doc.orig_id, 2, doc.size.width); } emit(key,doc.name) } function date_to_array(aDate) { var out = [], d = new Date(aDate); out.push(d.getFullYear()); out.push(d.getMonth()+1); out.push(d.getDate()); out.push(d.getHours()); out.push(d.getMinutes()); out.push(d.getSeconds()); out.push(d.getMilliseconds()); return out; }}"
+      }
     }
-    else {
-        console.log('DB type is touchdb');
-        design_doc = design_doc_touchdb;
+  };
+
+  var design_doc_touchdb = {
+    "views":{
+      "image_by_oid_w_variant":{
+      }, "image_by_ctime":{
+      }, "batch_by_ctime":{
+      }, "batch_by_oid_w_image":{
+      }
     }
+  };
+
+  var design_doc;
+
+  if (options.design_doc === 'couchdb') {
+    console.log('DB type is couchdb');
+    design_doc = design_doc_couchdb;
+  }
+  else {
+    console.log('DB type is touchdb');
+    design_doc = design_doc_touchdb;
+  }
 
 
+  /*
+   */
 
-    /*
-     */
+  var isUpdate = true;
 
-    var isUpdate = true;
-
-    async.waterfall(
-        [
-            function (next) {
-                console.log("attempting to get design doc with id: %s", design_doc_id);
-                db.get(design_doc_id, function (err, doc, hdr) {
-                    if (err) {
-                        console.log("design doc does not exist, inserting it...");
-                        isUpdate = false;
-                        db.insert(design_doc, design_doc_id, next);
-                    } else {
-                        next(null, doc, hdr);
-                    }
-                });
-            },
-            function (doc, hdr, next) {
-                if (isUpdate) {
-                    console.log('have design doc:', doc);
-                    console.log('updating doc w/ doc._rev: %s', doc._rev);
-                    db.insert(design_doc, {doc_name:design_doc_id, rev:doc._rev}, next);
-                }
-                else {
-                    next(null, doc, hdr);
-                }
-            }
-        ],
-        function (err, result, hdr) {
-            if (err) {
-                console.log('Unable to insert/update design doc: ', err);
-                callback(err);
-            }
-            console.log("result: %j", result);
-            // console.log("hdr: %j", hdr);
-            if (isUpdate) {
-                console.log("Successfully updated design doc");
-            } else {
-                console.log("Successfully inserted design doc");
-            }
-            callback(null,result);
+  async.waterfall(
+    [
+      function (next) {
+        console.log("attempting to get design doc with id: %s", design_doc_id);
+        db.get(design_doc_id, function (err, doc, hdr) {
+          if (err) {
+            console.log("design doc does not exist, inserting it...");
+            isUpdate = false;
+            db.insert(design_doc, design_doc_id, next);
+          } else {
+            next(null, doc, hdr);
+          }
+        });
+      },
+      function (doc, hdr, next) {
+        if (isUpdate) {
+          console.log('have design doc:', doc);
+          console.log('updating doc w/ doc._rev: %s', doc._rev);
+          db.insert(design_doc, {doc_name:design_doc_id, rev:doc._rev}, next);
         }
-    );
+        else {
+          next(null, doc, hdr);
+        }
+      }
+    ],
+    function (err, result, hdr) {
+      if (err) {
+        console.log('Unable to insert/update design doc: ', err);
+        callback(err);
+      }
+      console.log("result: %j", result);
+      // console.log("hdr: %j", hdr);
+      if (isUpdate) {
+        console.log("Successfully updated design doc");
+      } else {
+        console.log("Successfully inserted design doc");
+      }
+      callback(null, result);
+    }
+  );
 }
 
 /*
