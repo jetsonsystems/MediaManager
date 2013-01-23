e7f7a871a9d672797fe49720a76c21df3474f92b
Philippe Paravicini <philippe@paravicini.com>
2013-01-23 09:47:41 -0800

    fine-tune Event.js, complete integration test of ImportBatchFs

--- ImageService/lib/plm-image/Event.js
+++ ImageService/lib/plm-image/Event.js
@@ -3,14 +3,16 @@ var _ = require('underscore');
 require('mootools');
 
 
-// Wrapper class that stores a timestamp for the time at which an event is emitted
+// Wrapper class that stores the event type and a timestamp for the time 
+// at which an event is emitted 
 // -------------------------------------------------------------
 module.exports = new Class (
 {
-  initialize : function(arg)
+  initialize : function(aType, arg)
   {
     this.class_name = 'plm.Event';
     this.emitted_at = new Date();
+    this.type = aType;
     this.data = arg;
   }
 });
