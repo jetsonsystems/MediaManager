badd81215f4506b4528bc383401bc915418cedd6
Marek J. Ryniejski <marekjulian@gmail.com>
2013-01-24 18:07:11 -0800

    clean up console logging

--- MediaManagerAppConfig/lib/config.js
+++ MediaManagerAppConfig/lib/config.js
@@ -8,6 +8,23 @@ var config = {
     remote: {
       host: "72.52.106.218"
     }
+  },
+  logging: {
+    "appenders": [
+      {
+        "type": "console",
+        "category": "console"
+      },
+      {
+        "type": "file",
+        "filename": "var/log/plm-media-manager.log",
+        "backups": 10,
+        "category": ["plm.MediaManagerApp", "plm.MediaManagerAppSupport", "plm.ImageService"]
+      }
+    ],
+    "levels": { "plm.MediaManagerApp" : "TRACE",
+                "plm.ImageService" : "ERROR" },
+    "replaceConsole": false
   }
 };
 
